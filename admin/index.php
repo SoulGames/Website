cf0bd765478fa98366097dd6e2d9c0221c9fc3b6
StackNeverFlow <linus@linusl.de>
2020-05-08 13:58:47 +0200

    :pencil2: Fixed a typo

--- admin/index.php
+++ admin/index.php
@@ -10,12 +10,11 @@
     <?php
     if(isset($_POST["submit"])){
       require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
       $stmt->bindParam(":user", $_POST["username"]);
       $stmt->execute();
       $count = $stmt->rowCount();
       if($count == 1){
-        //Username ist frei
         $row = $stmt->fetch();
         if(password_verify($_POST["pw"], $row["PASSWORD"])){
           session_start();
