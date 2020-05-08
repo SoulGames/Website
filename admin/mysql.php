fbae4dce1e85b23fcb5ad0ca6fb4a5ead4892095
StackNeverFlow <support@linusl.de>
2020-05-08 14:14:25 +0200

    :pencil: Add a comment
    Add a comment

--- admin/mysql.php
+++ admin/mysql.php
@@ -3,9 +3,10 @@ $host = "localhost";
 $name = "adminboard";
 $user = "root";
 $passwort = "";
+// replace the data with your mysql data
 try{
     $mysql = new PDO("mysql:host=$host;dbname=$name", $user, $passwort);
 } catch (PDOException $e){
     echo "SQL Error: ".$e->getMessage();
 }
- ?>
+?>
