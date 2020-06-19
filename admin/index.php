6d4690d94a88b688d542f5e8e7df4af80d0f5bbe
StackNeverFlow <support@linusl.de>
2020-06-19 13:15:50 +0200

    :poop: Made some changes

--- admin/index.php
+++ admin/index.php
@@ -7,30 +7,29 @@
 	<link rel="stylesheet" type="text/css" href="../src/js/bootstrap/bootstrap.min.js">
   </head>
   <body>
-    <?php
-    if(isset($_POST["submit"])){
-      require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
-      $stmt->bindParam(":user", $_POST["username"]);
-      $stmt->execute();
-      $count = $stmt->rowCount();
-      if($count == 1){
-        $row = $stmt->fetch();
-        if(password_verify($_POST["pw"], $row["PASSWORD"])){
-          session_start();
-          $_SESSION["username"] = $row["USERNAME"];
-          header("Location: addChange/index.php");
-        } else {
-          echo "Der Login ist fehlgeschlagen";
-        }
-      } else {
-        echo "Der Login ist fehlgeschlagen";
-      }
-    }
-     ?>
-
     <div class="container mx-auto">
     <center><h1>Anmelden</h1></center>
+      <?php
+      if(isset($_POST["submit"])){
+        require("mysql.php");
+        $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
+        $stmt->bindParam(":user", $_POST["username"]);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        if($count == 1){
+          $row = $stmt->fetch();
+          if(password_verify($_POST["pw"], $row["PASSWORD"])){
+            session_start();
+            $_SESSION["username"] = $row["USERNAME"];
+            header("Location: addChange/index.php");
+          } else {
+            echo `<div class="alert alert-danger"><strong>Danger!</strong>Der Login ist fehlgeschlagen.</div>`;
+          }
+        } else {
+          echo `<div class="alert alert-danger"><strong>Danger!</strong>Der Login ist fehlgeschlagen.</div>`;
+        }
+      }
+      ?>
         <form action="index.php" method="post">
         <input class="form-control" type="text" name="username" placeholder="Username" required><br>
         <input class="form-control" type="password" name="pw" placeholder="Passwort" required><br>
